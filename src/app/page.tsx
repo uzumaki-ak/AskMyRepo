74c21d316b4e4c4f4fa24caad01cde568f12cf32
uzumaki-ak <anikydv38@gmail.com>
2026-02-04 03:22:55 +0530

    fixed redirectlink

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
+import Link from "next/link";
 import { Button } from "~/components/ui/button";
 
-
-
 export default async function Home() {
-  
-
   return (
- <Button className="text-red-400">hey sop</Button>
-
- 
+    <Link href="/dashboard" className="text-red-400">
+      hey sop
+    </Link>
   );
 }
-
